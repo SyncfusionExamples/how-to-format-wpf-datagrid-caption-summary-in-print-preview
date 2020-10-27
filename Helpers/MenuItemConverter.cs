d0bd0db0632aecda1dd3725dde6be00ad97c0133
Arulpriya <32356523+Arulpriya@users.noreply.github.com>
2020-10-27 09:32:03 +0530

    Add files via upload

---
+++ Helpers/MenuItemConverter.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.Data;
+using Syncfusion.UI.Xaml.Grid;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CaptionSummaryCustomization
+{
+    public class MenuItemConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.ToArray();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
